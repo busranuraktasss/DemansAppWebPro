6eaffc05cde78640888a3c1980abddd690a65bf2
Büşra Nur Aktaş <96480027+busranuraktasss@users.noreply.github.com>
2023-06-08 23:44:26 +0300

    Motivasyon Cümleleri sayfası bitti!

---
+++ DemansAppWebPro/DemansAppWebPro/Helper/DTO/MotivationSentences/showSentenceRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DemansAppWebPro.Helper.DTO.MotivationSentences
+{
+    public class showSentenceRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
