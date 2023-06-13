3fed3473f54fb37f1f2ecaecf4ccf7edc10d23a5
Büşra Nur Aktaş <96480027+busranuraktasss@users.noreply.github.com>
2023-06-13 22:25:06 +0300

    Picture is done!

---
+++ DemansAppWebPro/DemansAppWebPro/Helper/DTO/Pictures/showPicturesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace DemansAppWebPro.Helper.DTO.Pictures
+{
+    public class showPicturesRequest
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Text { get; set; }
+        public bool Status { get; set; }
+        
+    }
+}
